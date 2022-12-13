ad16c029af6adf0ca3e3df1b70a6fa6a03a9fff5
climboutonalimb <31755531+climboutonalimb@users.noreply.github.com>
2022-12-13 01:13:45 -0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'Climboutonalimb',
+  author: 'Dave Grothaus',
+  email: 'admin@climboutonalimb.net',
+  link: 'https://climboutonalimb-blog.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
